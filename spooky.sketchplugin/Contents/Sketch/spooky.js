d76b8fce13ee7ce3c85b6fb9e284c7130f942670
Elias Julian <eliasneverlost@gmail.com>
2018-10-30 16:51:31 -0500

    added sketchrunner support

--- spooky.sketchplugin/Contents/Sketch/spooky.js
+++ spooky.sketchplugin/Contents/Sketch/spooky.js
@@ -1,9 +1,158 @@
-var that=this;function __skpm_run(e,t){that.context=t;var r=function(e){var t={};function r(o){if(t[o])return t[o].exports;var n=t[o]={i:o,l:!1,exports:{}};return e[o].call(n.exports,n,n.exports,r),n.l=!0,n.exports}return r.m=e,r.c=t,r.d=function(e,t,o){r.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:o})},r.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},r.t=function(e,t){if(1&t&&(e=r(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var o=Object.create(null);if(r.r(o),Object.defineProperty(o,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var n in e)r.d(o,n,function(t){return e[t]}.bind(null,n));return o},r.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return r.d(t,"a",t),t},r.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},r.p="",r(r.s="./src/spooky.js")}({"./src/spooky.js":
+var that = this;
+function __skpm_run (key, context) {
+  that.context = context;
+
+var exports =
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./src/spooky.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./src/spooky.js":
 /*!***********************!*\
   !*** ./src/spooky.js ***!
   \***********************/
-/*! no static exports found */function(e,r,o){var n=o(/*! sketch */"sketch"),l=n.getSelectedDocument().selectedLayers.layers[0];l&&"Image"===l.type||n.UI.message("You need to select an image"),__mocha__.loadFrameworkWithName("Vision");var u=MLModel.compileModelAtURL_error(t.plugin.urlForResourceNamed("PumpkinCleaver.mlmodel"),null),i=MLModel.modelWithContentsOfURL_error(u,null),a=VNCoreMLModel.modelForMLModel_error(i,null),s=VNCoreMLRequest.alloc().initWithModel(a),c=CIImage.imageWithData(l.image.nsdata);if(VNImageRequestHandler.alloc().initWithCIImage_options(c,null).performRequests_error([s],null)){var d=s.results()[0],f="pumpkin"===String(d.identifier());n.UI.message(f?"🔪🎃 very  s p o o o o k y 🔪🎃 ":"☹️ more spooky, plz, thx ☹️")}else n.UI.message("Something went wrong 😕")},sketch:
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+var sketch = __webpack_require__(/*! sketch */ "sketch");
+
+var document = sketch.getSelectedDocument();
+var selectedLayer = document.selectedLayers.layers[0];
+
+if (!selectedLayer || selectedLayer.type !== 'Image') {
+  sketch.UI.message('You need to select an image');
+}
+
+__mocha__.loadFrameworkWithName('Vision'); // compile the model
+
+
+var compiledModelURL = MLModel.compileModelAtURL_error(context.plugin.urlForResourceNamed("PumpkinCleaver.mlmodel"), null); // load the compiled model
+
+var model = MLModel.modelWithContentsOfURL_error(compiledModelURL, null); // transform our model into a Vision model
+
+var vnModel = VNCoreMLModel.modelForMLModel_error(model, null); // create a an image analysis request that uses
+// our Core ML model to process images
+
+var request = VNCoreMLRequest.alloc().initWithModel(vnModel); // VNImageRequestHandler expects a "CIImage"
+// Luckily, we can create one from our image layer
+
+var ciImage = CIImage.imageWithData(selectedLayer.image.nsdata);
+var handler = VNImageRequestHandler.alloc().initWithCIImage_options(ciImage, null);
+var success = handler.performRequests_error([request], null);
+
+if (success) {
+  var bestEstimation = request.results()[0];
+  var isAPumpkin = String(bestEstimation.identifier()) === 'pumpkin';
+  sketch.UI.message(isAPumpkin ? "🔪🎃 very  s p o o o o k y 🔪🎃 " : "☹️ more spooky, plz, thx ☹️");
+} else {
+  sketch.UI.message('Something went wrong 😕');
+}
+
+/***/ }),
+
+/***/ "sketch":
 /*!*************************!*\
   !*** external "sketch" ***!
   \*************************/
-/*! no static exports found */function(e,t){e.exports=require("sketch")}});"default"===e&&"function"==typeof r?r(t):r[e](t)}that.onRun=__skpm_run.bind(this,"default");
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = require("sketch");
+
+/***/ })
+
+/******/ });
+  if (key === 'default' && typeof exports === 'function') {
+    exports(context);
+  } else {
+    exports[key](context);
+  }
+}
+that['VNImageRequestHandler'] = __skpm_run.bind(this, 'VNImageRequestHandler');
+that['onRun'] = __skpm_run.bind(this, 'default')
+
+//# sourceMappingURL=spooky.js.map
